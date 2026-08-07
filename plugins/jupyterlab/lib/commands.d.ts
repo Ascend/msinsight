57890c9f715b6eaad7076db71aaa4f81dae39fe0
codeasier <liuyekang@huawei.com>
2026-08-07 19:38:25 +0800

    remove generated Jupyter build artifacts

--- plugins/jupyterlab/lib/commands.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * The command IDs used by the mindstudio plugin.
- */
-export declare const createNew = "mindstudio:create-new";
-export declare const open = "mindstudio:open";
-export declare const close = "mindstudio:close";
